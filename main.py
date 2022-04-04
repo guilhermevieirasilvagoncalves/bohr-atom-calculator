24587b6e37124ddfdb327b614347575cc81e2687
Kenzo <79611160+Kenzo-Sugai@users.noreply.github.com>
2022-04-04 15:42:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import numpy as np
+from math import sqrt
+
+""""
+    @authors: Guilherme Vieira Silva Gonçalves, Kenzo Sugai, Daniel Alves Cunha, Bruna Pereira Paz
+    @course: Ciência da Computação, 3th semester
+    # ⌨️ with ❤️ using Visual Studio Code #
+"""
 
 while True:
     print(
@@ -6,6 +13,9 @@ while True:
     Selecione uma opção de entrada: 
     [1] Entrada de n
     [2] Entrada do ni e nf
+    [3] Calcular nf
+    [4] Calcular ni
+    [5] Encerrar programa
     """)
 
     #constantes
@@ -20,21 +30,21 @@ while True:
 
     D = int(input())
     if D == 1:
-        n = int(input("Valor de n: "))
+        n = float(input("Valor de n: "))
         r = pow(n , 2) * a0
-        v = (1/e0) * (e**2/2*ni*h)
+        v = (2.187 * pow(10,6)) / n
         k = (13.6)/pow(n, 2)
         u = -27.2/pow(n, 2)
-        l = h / (m * v)
-        et = -13.6/n
+        l = (h / (m * v))
+        et = -13.6 / pow(n, 2)
         print(
         """
-        r = {0}
-        v = {1}
-        l = {2} m
-        k = {3} eV
+        r = {0} m  
+        v = {1} m/s
+        λ = {2} m
+        k = {3} eV   
         u = {4} eV
-        et = {5} eV
+        En = {5} eV 
         """.format(np.format_float_scientific(r, precision = 2, exp_digits = 1), np.format_float_scientific(v, precision = 2, exp_digits = 1), np.format_float_scientific(l, precision = 2, exp_digits = 1), np.format_float_scientific(k, precision = 2, exp_digits = 1), np.format_float_scientific(u, precision = 2, exp_digits = 1), np.format_float_scientific(et, precision = 2, exp_digits = 1))
         )
     if D == 2:
@@ -51,4 +61,65 @@ while True:
         λ fóton = {1} m
         F fóton = {2} Hz
         """.format(np.format_float_scientific(et, precision = 2, exp_digits = 1), np.format_float_scientific(l, precision = 2, exp_digits = 1), np.format_float_scientific(F, precision = 2, exp_digits = 1)))
+    if D == 3:
+        print(
+        """
+        Escolha uma entrada:
+        
+        [1] Comprimento de onda
+        [2] Frequencia
+        """)
+        N = int(input())
+        if N == 1:
+            ni = int(input("Valor do n inicial: "))
+            l = float(input("Valor do comprimento de onda: "))
+            deltaE = H * c / l
+            Ei = -13.6 / pow(ni,2)
+            Ef = deltaE + Ei
+            n = sqrt(-13.6 / Ef)
+            print("""
+                nf = {0} 
+            """.format(np.format_float_scientific(n, precision = 2, exp_digits = 1)))
+
+        if N == 2:
+            ni = int(input("Valor do n inicial: "))
+            f = float(input("Valor da frequência: "))
+            deltaE = H * f
+            Ei = -13.6 / pow(ni,2)
+            Ef = deltaE + Ei
+            n = sqrt(-13.6 / Ef)
+            print("""
+                nf = {0} 
+            """.format(np.format_float_scientific(n, precision = 2, exp_digits = 1)))
     
+    if D == 4:
+        print(
+        """
+        Escolha uma entrada:
+        
+        [1] Comprimento de onda
+        [2] Frequencia
+        """)
+        N = int(input())
+        if N == 1:
+            nf = int(input("Valor do n final: "))
+            f = float(input("Valor do comprimento de onda: "))
+            deltaE = H * c / l
+            Ef = -13.6 / pow(nf, 2)
+            Ei = deltaE + Ef
+            nprint = sqrt(-13.6 / Ei)
+            print("""
+                ni = {0} 
+            """.format(np.format_float_scientific(nprint, precision = 2, exp_digits = 1)))
+        if N == 2:
+            nf = int(input("Valor do n final: "))
+            f = float(input("Valor da frequência: "))
+            deltaE = H * f
+            Ef = -13.6 / pow(nf, 2)
+            Ei = deltaE + Ef
+            nprint = sqrt(-13.6 / Ei)
+            print("""
+                ni = {0} 
+            """.format(np.format_float_scientific(nprint, precision = 2, exp_digits = 1)))
+    if D == 5:
+        break
